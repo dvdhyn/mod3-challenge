8292d2f4544293c2ef3421821bf4fa456f95de7f
yungtaco <dvhyun@gmail.com>
2023-10-23 16:39:59 -0400

    Added JavaScript comments

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // defines possible values for lowercase, uppercase, numbers, and special characters
+// ___had to move variables userPass and charBank to global scale to allow generatePassword() and writePassword() functions to operate properly___ //
 var lowCase=["abcdefghijklmnopqrstuvwxyz"];
 var upCase=["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var numb=["0123456789"];
@@ -9,8 +10,8 @@ var type=["-_=+~`|{}[];:'<>./?!@#$%^&*()"];
 var userPass="";
 var charBank="";
 
-// defined missing function from function writePassword()
-// needs user input for character length, case, and type
+// defined missing generatePassword() function from function writePassword()
+// created variables for each user input confirming character length, case, and types
 function generatePassword() {
   var charLength=prompt("How many characters will your password be? (must be 8-128 characters)");
   if (charLength < 8 || charLength > 128) {
@@ -34,12 +35,15 @@ function generatePassword() {
     charBank += type;
   }
 
+  // used to confirm correct items are being added to varialbe charBank
   console.log(charBank);
 
+  // for loop will continue to pick random values from variable charBank until desired character length is reached
   for (var i = 0; i < charLength; i++) {
     userPass=userPass += charBank.charAt(Math.floor(Math.random() * charBank.length));
   }
   console.log(userPass);
+  // opted for an alert and a return for userPass result
   alert("Your generated password is: " + userPass);
   return(userPass);
 }
